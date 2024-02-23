558d2450741389bb986bf2a2b21f1986c99ad320
Seungmin <sabaka.delaetGafGaf@gmail.com>
2024-02-23 22:10:01 +0100

    Dog and Bird

---
+++ main.py
@@ -0,0 +1,33 @@
+class Animal:
+    def __init__(self, name , species, legs):
+        self.name = input("name: ")
+        self.species = "animal"
+        self.legs = 4
+
+    def voice(self):
+        print("voice")
+
+    def runing(self):
+        print("Animal il runing")
+
+
+class Dog(Animal):
+    breed = "shepherd"
+
+    def bark(self, bark):
+        self.bark = "bark"
+
+
+class Bird(Animal):
+    wingspan = 50
+
+    def __init__(self, name, wingspan):
+        self.name = "Barsik"
+        self.wingspan = 50
+
+dog = Dog("shepherd")
+bird = Bird("Barsik")
+d = Dog
+b = Bird
+d.bark()
+b.wingspan()
